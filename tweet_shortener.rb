9d51f52afcbf13823f08ec0cf18bfd84f760c286
Andrew Bernier <a6bernier@gmail.com>
2018-09-12 17:41:39 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,3 +39,11 @@ def selective_tweet_shortener(array)
        array
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0...136]
+  else
+    tweet
+  end
+end
