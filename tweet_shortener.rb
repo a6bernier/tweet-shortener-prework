e4997cce0ea7b52c149f8d6cc85056528bbff4b2
Andrew Bernier <a6bernier@gmail.com>
2018-09-12 16:36:05 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,10 +1,16 @@
-dictionary = {
-  :hello => "hi" ,
-  :to => "2" ,
-  :two => "2" ,
-  :too => "2" ,
-  :be => "b" ,
-  :you => "u" ,
-  :at => "@" ,
-  :and => "&"
-}
+def dictionary
+  dictionary = {
+    :hello => "hi" ,
+    :to => "2" ,
+    :two => "2" ,
+    :too => "2" ,
+    :be => "b" ,
+    :you => "u" ,
+    :at => "@" ,
+    :and => "&"
+  }
+end
+
+def word_substituter(tweet)
+
+end
